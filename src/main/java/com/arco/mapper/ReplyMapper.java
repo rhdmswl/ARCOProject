14474498255aa1991dc837d27d87bccda32274fe
mine7272 <70158840+mine7272@users.noreply.github.com>
2023-04-13 14:24:50 +0900

    Merge pull request #34 from rhdmswl/minhoe
    Minhoe

---
+++ src/main/java/com/arco/mapper/ReplyMapper.java
@@ -0,0 +1,19 @@
+package com.arco.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import com.arco.domain.Criteria;
+import com.arco.domain.ReplyVO;
+
+public interface ReplyMapper {
+	
+	public int insert(ReplyVO vo);	
+	public ReplyVO read(Long com_id);
+	public int delete(Long com_id); 
+	public int update(ReplyVO reply);
+
+	public List<ReplyVO> getListWithPaging(
+	          @Param("cri") Criteria cri, 
+	          @Param("post_id") Long post_id);
+}
