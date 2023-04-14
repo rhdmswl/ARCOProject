7c69323001ca5f5341833f70cbaebfd1394c73a6
박정현 <ros28081@naver.com>
2023-04-14 16:44:59 +0900

    MUL-57,MUL-59,MUL-62,MUL-66 [FEAT] MemberVO 추가

---
+++ src/main/java/com/collection/vo/MemberVO.java
@@ -0,0 +1,19 @@
+package com.collection.vo;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String userId;
+	private String userPass;
+	private String userName;
+	private String phone;
+	private String email;
+	private String profileImg;
+
+	private List<CollectionReviewVO> collectionRevs;
+	private List<BoardVO> posts;
+	private List<ReplyVO> comments;
+}
