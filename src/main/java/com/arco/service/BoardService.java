7e47546266e7420af627bf2e2980b223df75490d
Yongbb_13 <91677796+KimMonkey2945@users.noreply.github.com>
2023-04-19 15:45:31 +0900

    Merge pull request #81 from rhdmswl/0419작업_페이징_별점_조회수
    0419

--- src/main/java/com/arco/service/BoardService.java
+++ src/main/java/com/arco/service/BoardService.java
@@ -2,8 +2,6 @@ package com.arco.service;
 
 import java.util.List;
 
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
 import com.arco.domain.BoardVO;
 import com.arco.domain.Criteria;
      
