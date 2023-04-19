e0f04ec901d25811f1fc5fc842e616e068bb3bf6
JISU <dig06177@naver.com>
2023-04-19 09:53:42 +0900

    MUL-80 [Add] 게시판 별 카테고리 이동 기능 추가

--- src/main/java/com/arco/service/BoardServiceImpl.java
+++ src/main/java/com/arco/service/BoardServiceImpl.java
@@ -2,7 +2,6 @@ package com.arco.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.arco.domain.BoardVO;
@@ -17,7 +16,6 @@ import lombok.extern.log4j.Log4j;
 @AllArgsConstructor
 public class BoardServiceImpl implements BoardService {
 	
-	@Autowired
 	private BoardMapper mapper;
 	
 	@Override
