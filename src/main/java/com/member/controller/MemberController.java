fb5042f3b2add77c5b044efe727d37a947dac14e
박정현 <ros28081@naver.com>
2023-04-22 13:47:41 +0900

    MUL-27 [Fix] 마이페이지 한줄평에서 전시 seq를 받아오는 코드 추가

--- src/main/java/com/member/controller/MemberController.java
+++ src/main/java/com/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.member.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
@@ -25,6 +26,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.member.service.MemberService;
+import com.member.vo.CollectionReviewVO;
 import com.member.vo.Criteria;
 import com.member.vo.ImageVO;
 import com.member.vo.MemberVO;
@@ -109,12 +111,6 @@ public class MemberController {
 		return "redirect:/";
 	}
 
-//	// 회원정보 수정 get
-//	@RequestMapping(value = "/memberUpdateView", method = RequestMethod.GET)
-//	public String registerUpdateView() throws Exception {
-//		return "member/memberUpdateView";
-//	}
-
 	// 회원 탈퇴 get
 	@RequestMapping(value = "/memberDeleteView", method = RequestMethod.GET)
 	public String memberDeleteView() throws Exception {
@@ -130,8 +126,6 @@ public class MemberController {
 
 		return "redirect:/";
 	}
-
-	
 	
 	// mypage - 나의 글 보기 (페이징 적용)
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
@@ -155,13 +149,19 @@ public class MemberController {
 	    postCri.setUserId(userId);
 	    commentCri.setUserId(userId);
 
-	    model.addAttribute("collectionRevs", service.getMemberCollectionRevsWithPaging(userId, revCri));
+	    model.addAttribute("collectionRev", service.getMemberCollectionRevsWithPaging(userId, revCri));
 	    model.addAttribute("posts", service.getMemberPostsWithPaging(userId, postCri));
 	    model.addAttribute("comments", service.getMemberCommentsWithPaging(userId, commentCri));
 
 	    PageMaker collectionRevPageMaker = new PageMaker(revCri, service.countCollectionRevs(userId));
 	    PageMaker postPageMaker = new PageMaker(postCri, service.countPosts(userId));
 	    PageMaker commentPageMaker = new PageMaker(commentCri, service.countComments(userId));
+	    
+	    List<CollectionReviewVO> collectionReviews = service.getMemberCollectionRevsWithPaging(userId, revCri);
+	    for (CollectionReviewVO review : collectionReviews) {
+	        review.setCollectionSeq(review.getCollectionSeq());
+	    }
+	    model.addAttribute("collectionRev", collectionReviews);
 
 	    model.addAttribute("collectionRevPageMaker", collectionRevPageMaker);
 	    model.addAttribute("postPageMaker", postPageMaker);
@@ -179,17 +179,11 @@ public class MemberController {
 		
 		
 	    service.mypageUpdate(vo);
-	   
 	    session.setAttribute("member", vo);
-	    
 	    model.addAttribute("member", vo);
 		
 	    return "redirect:/member/mypage";
 	}
-
-
-
-
 
 	@RequestMapping(value = "/passUpdateView", method = RequestMethod.GET)
 	public String pwUpdateView() throws Exception {
